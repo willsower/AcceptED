37847a5c2e1fd864be10144096bc5689e70ee75e
Taichen <jatr812@gmail.com>
2021-04-13 10:49:50 -0700

    Have onboardingOne footer created

---
+++ accepted/components/footer.js
@@ -0,0 +1,12 @@
+
+const Footer = () => {
+    return (
+        <div className = 'absolute inset-x-0 bottom-0 h-16'>
+            <div className="absolute text-gray-500 bottom-0 right-0 h-16 ...">CONTACT US</div>
+
+            <div className="absolute text-gray-500 bottom-0 left-0 h-16  ...">ACCEPTED &copy;2021</div>
+        </div>
+    )
+}
+
+export default Footer
