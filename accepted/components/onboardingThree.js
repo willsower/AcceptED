d4ff6a39f7fbd3be2ecc694d550aefb69d6ac00c
Taichen <jatr812@gmail.com>
2021-04-13 14:22:42 -0700

    Somewhat finished third onboarding

--- accepted/components/onboardingThree.js
+++ accepted/components/onboardingThree.js
@@ -7,21 +7,32 @@ function OnboardingThree() {
     return (
         <div className = 'min-h-screen relative'>
             <div className = 'm-auto w-4/5 pt-4 flex flex-col pb-20'>
-                <h1 className = 'text-3xl text-blue-600 mb-2 text-center'>Welcome, [Name]!</h1>
-                <h3 className = "text-xl font-semibold text-center">Watch a short training video</h3>
-                <h5 className = 'text-center text-base mt-4'>How to Engage Your Student</h5>
+                <h3 className = "text-xl font-semibold text-center">Please upload the following documents</h3>
 
                 <div className= 'm-auto'>
+                    <h5 className = 'text-center font-semibold text-base mt-4'>Document #1</h5>
 
+                    <div className="bg-white px-2">
+                        <div className="max-w-md mx-auto rounded-lg overflow-hidden md:max-w-xl">
+                            <div className="md:flex">
+                                <div className="w-32 p-3 m-auto">
+                                    <div className="relative border-dotted h-24 rounded-lg border-dashed border-2 border-gray-300 bg-gray-100 flex justify-center items-center">
+                                        <div className="absolute">
+                                            <div className="flex flex-col items-center"> 
+                                                <i className="fa fa-folder-open fa-4x text-blue-700"></i> 
+                                                <span className="block text-gray-400 font-normal">Upload</span> </div>
+                                        </div> 
+                                        <input type="file" className="h-full w-full opacity-0 cursor-pointer" name=""/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
                 <button className = 'bg-blue-600 text-white rounded w-28 p-1 ml-auto mr-auto mt-8'>
                         Next
                 </button>
-
-                <Link href="">
-                    <p className = 'text-xs mt-2 underline ml-auto mr-auto'><a className = "text-blue-600">Skip for now</a></p>
-                </Link>
             </div>
 
             <Footer />
