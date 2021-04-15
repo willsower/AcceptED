065bd0a7200a9385bfc360926f108d396dcbb7da
Taichen <jatr812@gmail.com>
2021-04-15 11:33:12 -0700

    Added dashboard shell

--- accepted/components/nav.js
+++ accepted/components/nav.js
@@ -1,8 +1,53 @@
+import Link from 'next/link';
+
 // Onboarding Footer
 function Nav() {
     return (
         <>
+            {/* Profile Picture */}
+            <div className = 'mt-12'>
+                <div className = 'box-content w-4 h-4 p-4 bg-gray-50 rounded-full m-auto'>
+                    
+                </div>
+            </div>
 
+            {/* Navigation */}
+            <div className = 'mt-16'>
+                {/* Home */}
+                <Link href = '/'>
+                    <div className = 'box-content w-3 h-3 p-4 bg-gray-50 rounded-sm m-auto mb-8'>
+                            
+                    </div>
+                </Link>
+
+                {/* Message */}
+                <Link href = '/'>
+                    <div className = 'box-content w-3 h-3 p-4 bg-gray-50 rounded-sm m-auto mb-8'>
+                        
+                    </div>
+                </Link>
+
+                {/* Materials */}
+                <Link href = '/'>
+                    <div className = 'box-content w-3 h-3 p-4 bg-gray-50 rounded-sm m-auto mb-8'>
+                        
+                    </div>
+                </Link>
+
+                {/* Writing */}
+                <Link href = '/'>
+                    <div className = 'box-content w-3 h-3 p-4 bg-gray-50 rounded-sm m-auto mb-8'>
+                        
+                    </div>
+                </Link>
+
+                {/* Profile */}
+                <Link href = '/'>
+                    <div className = 'box-content w-3 h-3 p-4 bg-gray-50 rounded-sm m-auto mb-8'>
+                        
+                    </div>
+                </Link>
+            </div>
         </>
     );
 }
--- accepted/pages/dashboardShell.js
+++ accepted/pages/dashboardShell.js
@@ -2,13 +2,35 @@ import Head from 'next/head';
 import Nav from '../components/nav';
 import Header from '../components/header';
 
-export default function Home() {
+export default function DashboardShell() {
   return (
     <>
         <Head>
         {/* <title>{ siteTitle }</title> */}
             <title>Test</title>
+            <link rel='icon' href='/favicon.ico' />
         </Head>
+        <main>
+            <div className='flex flex-row'>
+                {/* Navbar - Left */}
+                <div className='w-20 h-screen flex flex-col relative bg-red-200'>
+                    <Nav />
+                </div>
+
+                {/* Right Side of Screen */}
+                <div className='flex-grow h-screen flex flex-col relative bg-green-200'>
+                    {/* Header Top */}
+                    <div className='h-20 flex flex-col relative bg-blue-200'>
+                        <Header />
+                    </div>
+
+                    {/* Content Bottom */}
+                    <div className='flex-grow h-screen flex flex-col relative bg-gray-200'>
+                        
+                    </div>
+                </div>
+            </div>
+        </main>
     </>
   );
 }
