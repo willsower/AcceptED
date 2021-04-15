d5f661d11bfc44dfadbffa40071da823fb6f7450
Taichen <jatr812@gmail.com>
2021-04-15 12:21:12 -0700

    Got basic header in place

--- accepted/components/header.js
+++ accepted/components/header.js
@@ -2,6 +2,15 @@
 function Header() {
     return (
         <>
+            <div className = 'mt-auto mb-auto ml-8'>
+                <div className = 'inline-block mr-8'>
+                    <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M20 .755l-14.374 11.245 14.374 11.219-.619.781-15.381-12 15.391-12 .609.755z"/></svg>
+                </div>
+                
+                <div className = 'text-2xl font-semibold text-red-900 inline-block'>
+                    Student 1 Progress
+                </div>
+            </div>
         </>
     );
 }
