ee2c4602c7cd0247a05d26e0b860bb95eedec5c9
Taichen <jatr812@gmail.com>
2021-04-23 17:32:55 -0700

    Updated sign up page

--- accepted/pages/signup.js
+++ accepted/pages/signup.js
@@ -14,13 +14,22 @@ export default function SignUp({children, home}) {
             <main>
                 <div className='flex flex-row h-full'>
                     {/* Image - Left */}
-                    <div className='w-2/5 h-screen flex justify-center flex-col relative'>
+                    <div className='w-2/5 h-screen flex justify-center flex-col relative items-center'>
                         <Image
-                            src='/images/signup.jpg'
+                            src='/images/signupTemp.jpeg'
                             alt='Picture of the author'
                             layout='fill'
                             objectFit='cover'
                         />
+                        <div className = 'absolute relative block'>
+                            <div className = 'block mb-12 sm:mb-16 md:mb-24'>
+                                <h1 className = 'text-xl md:text-3xl lg:text-4xl text-gray-50 font-semibold'>Welcome Student!</h1>
+                                <h3 className = 'text-base md:text-xl text-gray-50 mt-4 font-semibold'>Let's get you started!</h3>
+                                <h4 className = 'text-base md:text-xl text-gray-50 mt-12'>Sign Up</h4>
+                                <h4 className = 'text-base md:text-xl text-gray-50 mt-7'>Watch Training Videos</h4>
+                                <h4 className = 'text-base md:text-xl text-gray-50 mt-7'>Upload Onboarding Files</h4>
+                            </div>
+                        </div>
                     </div>
 
                     {/* Sign Up Screen - Right */}
@@ -30,22 +39,21 @@ export default function SignUp({children, home}) {
                             LOGO
                         </div>
                         <div className = 'm-auto w-8/12'>
-                            <h1 className = 'text-xl md:text-3xl text-blue-600 mb-2'>Welcome Consultant!</h1>
-                            <h3 className = 'text-base md:text-xl font-semibold'>Let's get you started!</h3>
+                            <h3 className = 'text-base md:text-2xl font-semibold mb-8'>Sign Up</h3>
 
                             {/* Sign-Up Form */}
                             <form className='flex flex-col mt-2'>
-                                <p className = 'text-sm md:text-base'>Full name</p>
-                                <input className='text-xs md:text-base bg-gray-200 rounded p-1 flex-1 mb-4' id='fullName' type='fullName'/>
+                                <p className = 'text-sm md:text-base font-semibold'>Full Name</p>
+                                <input className='text-xs md:text-sm bg-gray-50 rounded p-2 flex-1 mb-4 border' id='fullName' type='fullName' placeholder='Enter first and last name'/>
 
-                                <p className = 'text-sm md:text-base'>School email address</p>
-                                <input className='text-xs md:text-base bg-gray-200 rounded p-1 flex-1 mb-4' id='email' type='email' aria-label='email address'/>
+                                <p className = 'text-sm md:text-base font-semibold'>Email</p>
+                                <input className='text-xs md:text-sm bg-gray-50 rounded p-2 flex-1 mb-4 border' id='email' type='email' aria-label='email address' placeholder='Enter school email address'/>
 
-                                <p className = 'text-sm md:text-base'>University code</p>
-                                <input className=' text-xs md:text-base bg-gray-200 rounded p-1 flex-1 mb-4' id='fullName' type='fullName'/>
+                                <p className = 'text-sm md:text-base font-semibold'>University Code</p>
+                                <input className=' text-xs md:text-sm bg-gray-50 rounded p-2 flex-1 mb-4 border' id='fullName' type='fullName' placeholder='Enter 4 digit university code'/>
 
-                                <p className = 'text-sm md:text-base'>Password (6-18 charachters)</p>
-                                <input className=' text-xs md:text-base bg-gray-200 rounded p-1 flex-1 mb-4' id='fullName' type='fullName'/>
+                                <p className = 'text-sm md:text-base font-semibold'>Password</p>
+                                <input className=' text-xs md:text-sm bg-gray-50 rounded p-2 flex-1 mb-4 border' id='fullName' type='fullName' placeholder='Enter password(6-8 charachters)'/>
 
                                 <Link href= {{ pathname:'onboarding'}}>
                                     <button className = 'w-20 bg-blue-600 text-white rounded md:w-28 p-1 m-auto'>
