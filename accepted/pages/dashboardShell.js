970f178e8a5cecbaa559bd60313dfd6e85f59ee2
Taichen <jatr812@gmail.com>
2021-05-05 18:08:56 -0700

    Have basic routing finsihed

--- accepted/pages/dashboardShell.js
+++ accepted/pages/dashboardShell.js
@@ -1,175 +1,197 @@
 import Head from 'next/head';
-import Link from 'next/link';
 import Image from 'next/image';
+import React from 'react'
 
-// Test
-import CounselorProfile from './Profiles/counselorProfile';
+import Home from './home'
 import Messages from './messages';
+import Materials from './materials'
+import Tasks from './tasks'
+import CounselorProfile from './Profiles/counselorProfile';
 
+const pages = [CounselorProfile, Messages];
 
 export default function DashboardShell() {
-  return (
-    <>
-        <Head>
-        {/* <title>{ siteTitle }</title> */}
-            <title>Test</title>
-            <link rel='icon' href='/favicon.ico' />
-        </Head>
-        <main>
-            <div className='flex flex-row'>
+    const { useState } = React;
+    const [currentPage, setCurrentPage] = useState('Home');
 
-                {/* Navbar - Left */}
-                <div className='sm:w-36 h-screen flex flex-col relative'>
-                    <div className = 'fixed border-r border-gray-200 sm:w-36 h-screen'>
-                        {/* Profile Picture */}
-                        <div className = 'mt-2'>
-                            <div className = 'p-4 sm:pl-9 sm:pr-9'>
-                                <Image
-                                    priority
-                                    src="/images/logo_temp.png"
-                                    className = ''
-                                    height={100}
-                                    width={100}
-                                />
+    const renderPage = () => {
+        if (currentPage == 'Home') {
+            return <Home />
+        } else if (currentPage == 'Messages') {
+            return <Messages />;
+        } else if (currentPage == 'Materials') {
+            return <Materials />;
+        } else if (currentPage == 'Tasks') {
+            return <Tasks />;
+        } else if (currentPage == 'Profile') {
+            return <CounselorProfile />;
+        }
+    }
+
+    return (
+        <>
+            <Head>
+            {/* <title>{ siteTitle }</title> */}
+                <title>{currentPage} | AcceptED</title>
+                <link rel='icon' href='/favicon.ico' />
+            </Head>
+            <main>
+                <div className='flex flex-row'>
+
+                    {/* Navbar - Left */}
+                    <div className='sm:w-36 h-screen flex flex-col relative'>
+                        <div className = 'fixed border-r border-gray-200 sm:w-36 h-screen'>
+                            {/* AcceptED Logo */}
+                            <div className = 'mt-2'>
+                                <div className = 'p-4 sm:pl-9 sm:pr-9'>
+                                    <Image
+                                        priority
+                                        src="/images/logo_temp.png"
+                                        className = ''
+                                        height={100}
+                                        width={100}
+                                    />
+                                </div>
                             </div>
-                        </div>
 
-                        {/* Navigation */}
-                        <div className = ''>
-                            {/* Home */}
-                            <Link href = '/'>
+                            {/* Navigation */}
+                            <div className = ''>
+                                {/* Home -> 0*/}
                                 <div className = 'w-full sm:h-12 p-2 sm:p-4 m-auto align-middle hover:bg-blue-50 active:bg-blue-50'>
-                                    <div className = 'inline-block align-middle mr-1 sm:mr-3'>
-                                        <Image
-                                            priority
-                                            src="/images/home_temp.png"
-                                            className = ''
-                                            height={20}
-                                            width={20}
-                                        />
-                                    </div>
-                                    <div className = 'text-xs sm:text-base inline-block text-gray-400'>
-                                        Home
-                                    </div>
+                                    <button className = 'focus:outline-none' onClick={() => setCurrentPage('Home')}>
+                                        <div className = 'inline-block align-middle mr-1 sm:mr-3'>
+                                            <Image
+                                                priority
+                                                src="/images/home_temp.png"
+                                                className = ''
+                                                height={20}
+                                                width={20}
+                                            />
+                                        </div>
+                                        <div className = 'text-xs sm:text-base inline-block text-gray-400'>
+                                            Home
+                                        </div>
+                                    </button>
                                 </div>
-                            </Link>
 
-                            {/* Message */}
-                            <Link href = '/'>
+                                {/* Message */}
                                 <div className = 'w-full sm:h-12 p-2 sm:p-4 m-auto hover:bg-blue-50 active:bg-blue-50'>
-                                    <div className = 'inline-block align-middle mr-1 sm:mr-3'>
-                                        <Image
-                                            priority
-                                            src="/images/messages_temp.png"
-                                            className = ''
-                                            height={20}
-                                            width={20}
-                                        />
-                                    </div>
-                                    <div className = 'text-xs sm:text-base inline-block text-gray-400'>
-                                        Messages
-                                    </div>
+                                    <button className = 'focus:outline-none' onClick={() => setCurrentPage('Messages')}>
+                                        <div className = 'inline-block align-middle mr-1 sm:mr-3'>                                            
+                                            <Image
+                                                priority
+                                                src="/images/messages_temp.png"
+                                                className = ''
+                                                height={20}
+                                                width={20}
+                                            />
+                                        </div>
+                                        <div className = 'text-xs sm:text-base inline-block text-gray-400'>
+                                            Messages
+                                        </div>
+                                    </button>
                                 </div>
-                            </Link>
 
-                            {/* Materials */}
-                            <Link href = '/'>
+                                {/* Materials */}
                                 <div className = 'w-full sm:h-12 p-2 sm:p-4 m-auto hover:bg-blue-50 active:bg-blue-50'>
-                                    <div className = 'inline-block align-middle mr-1 sm:mr-3'>
-                                        <Image
-                                            priority
-                                            src="/images/materials_temp.png"
-                                            className = ''
-                                            height={20}
-                                            width={20}
-                                        />
-                                    </div>
-                                    <div className = 'text-xs sm:text-base inline-block text-gray-400'>
-                                        Materials
-                                    </div>
+                                    <button className = 'focus:outline-none' onClick={() => setCurrentPage('Materials')}>
+                                        <div className = 'inline-block align-middle mr-1 sm:mr-3'>
+                                            <Image
+                                                priority
+                                                src="/images/materials_temp.png"
+                                                className = ''
+                                                height={20}
+                                                width={20}
+                                            />
+                                        </div>
+                                        <div className = 'text-xs sm:text-base inline-block text-gray-400'>
+                                            Materials
+                                        </div>
+                                    </button>
                                 </div>
-                            </Link>
 
-                            {/* Tasks */}
-                            <Link href = '/'>
+                                {/* Tasks */}
                                 <div className = 'w-full sm:h-12 p-2 sm:p-4 m-auto hover:bg-blue-50 active:bg-blue-50'>
-                                    <div className = 'inline-block align-middle mr-1 sm:mr-3'>
-                                        <Image
-                                            priority
-                                            src="/images/tasks_temp.png"
-                                            className = ''
-                                            height={20}
-                                            width={20}
-                                        />
-                                    </div>
-                                    <div className = 'text-xs sm:text-base inline-block text-gray-400'>
-                                        Tasks
-                                    </div>
+                                    <button className = 'focus:outline-none' onClick={() => setCurrentPage('Tasks')}>
+                                        <div className = 'inline-block align-middle mr-1 sm:mr-3'>
+                                            <Image
+                                                priority
+                                                src="/images/tasks_temp.png"
+                                                className = ''
+                                                height={20}
+                                                width={20}
+                                            />
+                                        </div>
+                                        <div className = 'text-xs sm:text-base inline-block text-gray-400'>
+                                            Tasks
+                                        </div>
+                                    </button>
                                 </div>
-                            </Link>
 
-                            {/* Profile */}
-                            <Link href = '/dashboardShell'>
+                                {/* Profile*/}
                                 <div className = 'w-full sm:h-12 p-2 sm:p-4 m-auto hover:bg-blue-50 active:bg-blue-50'>
-                                    <div className = 'inline-block align-middle mr-1 sm:mr-3'>
-                                        <Image
-                                            priority
-                                            src="/images/profileico_temp.png"
-                                            className = ''
-                                            height={20}
-                                            width={20}
-                                        />
-                                    </div>
-                                    <div className = 'text-xs sm:text-base inline-block text-gray-400'>
-                                        Profile
-                                    </div>
+                                    <button className = 'focus:outline-none' onClick={() => setCurrentPage('Profile')}>
+                                        <div className = 'inline-block align-middle mr-1 sm:mr-3'>                                            
+                                            <Image
+                                                priority
+                                                src="/images/profileico_temp.png"
+                                                className = ''
+                                                height={20}
+                                                width={20}
+                                            />
+                                        </div>
+                                        <div className = 'text-xs sm:text-base inline-block text-gray-400'>
+                                            Profile
+                                        </div>
+                                    </button>
                                 </div>
-                            </Link>
 
-                            {/* Logout */}
-                            <Link href = '/'>
+                                {/* Logout*/}
                                 <div className = 'w-full sm:h-12 p-2 sm:p-4 m-auto hover:bg-blue-50 active:bg-blue-50'>
-                                    <div className = 'inline-block align-middle mr-1 sm:mr-3'>
-                                        <Image
-                                            priority
-                                            src="/images/logout_temp.png"
-                                            className = ''
-                                            height={20}
-                                            width={20}
-                                        />
-                                    </div>
-                                    <div className = 'text-xs sm:text-base inline-block text-gray-400'>
-                                        Log Out
-                                    </div>
-                                </div>
-                            </Link>
-                        </div>
-                    </div>
-                </div>
-
-                {/* Right Side of Screen */}
-                <div className='flex-grow h-screen flex flex-col relative'>
-
-                    {/* Header Top */}
-                    <div className='h-16 flex flex-col relative'>
-                        <div className = 'border-b border-gray-200 h-16 w-full z-10 fixed bg-white'>
-                            <div className = 'mt-auto mb-auto p-3 ml-2'>                
-                                <div className = 'text-2xl font-semibold inline-block'>
-                                    Profile
+                                    <button className = 'focus:outline-none' onClick={() => setCurrentPage('Logout')}>
+                                        <div className = 'inline-block align-middle mr-1 sm:mr-3'>
+                                            <Image
+                                                priority
+                                                src="/images/logout_temp.png"
+                                                className = ''
+                                                height={20}
+                                                width={20}
+                                            />
+                                        </div>
+                                        <div className = 'text-xs sm:text-base inline-block text-gray-400'>
+                                            Log Out
+                                        </div>
+                                    </button>
                                 </div>
                             </div>
                         </div>
                     </div>
 
-                    {/* Content Bottom */}
-                    <div className='flex-grow h-auto flex flex-col relative bg-gray-50 pt-12 pb-12 z-0'>
-                        {/* Test */}
-                        {/* <CounselorProfile /> */}
-                        <Messages />
+                    {/* Right Side of Screen */}
+                    <div className='flex-grow h-screen flex flex-col relative'>
+
+                        {/* Header Top */}
+                        <div className='h-16 flex flex-col relative'>
+                            <div className = 'border-b border-gray-200 h-16 w-full z-10 fixed bg-white'>
+                                <div className = 'mt-auto mb-auto p-3 ml-2'>                
+                                    <div className = 'text-2xl font-semibold inline-block'>
+                                        {currentPage}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        {/* Content Bottom */}
+                        <div className='flex-grow h-auto flex flex-col relative bg-gray-50 pt-12 pb-12 z-0'>
+                            {/* Test */}
+                            {/* <CounselorProfile /> */}
+                            {/* <Messages /> */}
+                            
+                            {renderPage()}
+                        </div>
                     </div>
                 </div>
-            </div>
-        </main>
-    </>
-  );
+            </main>
+        </>
+    );
 }
---
+++ accepted/pages/materials.js
@@ -0,0 +1,7 @@
+function Materials() {
+    return (
+        <h1>Materials Page</h1>
+    );
+}
+
+export default Materials;
