d59c8d02551296468374c82a2b8c45924599d38e
Taichen Rose <43691880+willsower@users.noreply.github.com>
2021-05-17 11:40:45 -0700

    Revert "Create pages"

---
+++ accepted/components/header.js
@@ -0,0 +1,18 @@
+// Onboarding Footer
+function Header() {
+    return (
+        <>
+            <div className = 'mt-auto mb-auto ml-8'>
+                <div className = 'inline-block mr-8'>
+                    <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd"><path d="M20 .755l-14.374 11.245 14.374 11.219-.619.781-15.381-12 15.391-12 .609.755z"/></svg>
+                </div>
+                
+                <div className = 'text-2xl font-semibold text-red-900 inline-block'>
+                    Profile
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Header;
