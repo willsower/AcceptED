462b8af041d0da1f72957d8c5e09ca5e2ecb1d0c
Taichen <jatr812@gmail.com>
2021-06-09 08:02:44 -0700

    Refactored index page back to function component

--- accepted/pages/index.js
+++ accepted/pages/index.js
@@ -2,18 +2,14 @@ import Head from 'next/head';
 import SignUp, { siteTitle } from './signup';
 import React, { Component } from 'react'
 
-class Index extends Component {
-  render() {
-    return (
-      <div>
-        <SignUp home>
-          <Head>
-            <title> {siteTitle} </title>
-          </Head>
-        </SignUp>
-      </div>
-    )
-  }
+export default function Home() {
+  return (
+    <div>
+      <SignUp home>
+        <Head>
+          <title> {siteTitle} </title>
+        </Head>
+      </SignUp>
+    </div>
+  )
 }
-
-export default Index;
