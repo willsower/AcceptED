355ff6cf06979b2f5ba80961df51a839d76f4bf9
Taichen <jatr812@gmail.com>
2021-06-10 03:43:20 -0700

    Got login and sign up page finished at minimum level of functionality

--- accepted/pages/login_signup.js
+++ accepted/pages/login_signup.js
@@ -33,7 +33,7 @@ export default function LoginSignup ({children, home}) {
                             <h1 className = 'text-xl md:text-3xl lg:text-4xl text-gray-50 font-semibold'>Welcome Consultant!</h1>
                             <h3 className = 'text-base md:text-xl text-gray-50 mt-4 font-semibold'>Let's get you started!</h3>
 
-                            <div clasName = ''>
+                            <div className = ''>
                                 <div className = 'inline-block mr-4'>
                                     <Image
                                         src='/images/SignUp/FileText.svg'
@@ -74,20 +74,23 @@ export default function LoginSignup ({children, home}) {
                     {/* Sign Up Screen - Right */}
                     <div className='w-3/5 h-screen flex flex-col relative'>
                         {/* Logo */}
-                        <div className ='absolute top-0 right-0 h-16 w-16 mt-2'>
-                            LOGO
+                        <div className ='absolute top-2 right-2 h-10 w-10 mt-2'>
+                            <Image
+                                src = '/images/accepted_logo.svg'
+                                layout = 'fill'
+                            />
                         </div>
                         <div className = 'm-auto w-8/12'>
-                            <h3 className = 'text-base md:text-2xl font-semibold mb-8'>Login</h3>
+                            <h3 className = 'text-base md:text-2xl font-semibold mb-2'>Login</h3>
                             {!session && <>
-                                Not signed in <br/>
-                                <button className = 'signInButton' onClick={() => signIn('google', { callbackUrl: 'http://localhost:3000/home' })}>Login</button>
+                                {/* Not signed in <br/> */}
+                                <Image src = '/images/Icon/google_signin.png' width = '150' height = '40' className = 'cursor-pointer' onClick={() => signIn('google', { callbackUrl: 'http://localhost:3000/home' })}/>
                             </>}
-
-                            <h3 className = 'text-base md:text-2xl font-semibold mb-8'>Sign Up</h3>
+                            {/* <Image src="/images/Icon/google_signin.png" alt="me" width="150" height="45" /> */}
+                            <h3 className = 'text-base md:text-2xl font-semibold mb-2 mt-4'>Sign Up</h3>
                             {!session && <>
-                                Not signed in <br/>
-                                <button className = 'signInButton' onClick={() => signIn('google', { callbackUrl: 'http://localhost:3000/create_account' })}>Signup</button>
+                                {/* Not signed in <br/> */}
+                                <Image src = '/images/Icon/google_signin.png' width = '150' height = '40' className = 'cursor-pointer' onClick={() => signIn('google', { callbackUrl: 'http://localhost:3000/create_account' })}/>
                             </>}
                         </div>
                     </div>
